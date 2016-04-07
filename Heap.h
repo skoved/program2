d5e82ddebb33d74567aba4fb622c0547b37174f4
skoved <sam.koved@gmail.com>
2016-04-07 13:46:24 -0400

    initial coomit

---
+++ Heap.h
@@ -0,0 +1,19 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include "Player.h"
+#include <string>
+#include <vector>
+
+class Heap{
+    private:
+        vector<int> heap;
+    public:
+        Heap(std::string filename);//creates a heap by reading in players from a file
+        Heap(const Heap &copy);//creates a new heap that is identical to the heap that gets passed
+        void addPlayer(Player newPlayer);//adds a new player to the heap
+        Player getPlayer();//returns the player with the hight priority
+        bool empty();//returns true if there are no elements in the heap, otherwise returns false
+        unsigned int size();//returns the number of players in the heap
+        void heapify();//formats the vector into a heap
+        void siftDown(int index);//performs siftDown on the passed index
+};
---
+++ Player.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "Card.h"
+#include <string>
+
+class Player{
+    private:
+        int budget;
+        std::string name;
+    public:
+        Card hand;
+        Player(std::string name, int budget);//creates a player with a name and budget
+        std::string getName();//returns the name of the player
+        int getBudget();//returns the budget of the player
+        bool bet(int amount);//the player bets the amount passed, returns false if the player cannot bet the amount
+        void collectWinnings(int amount);//adds winnings to the players budget
+};
+
